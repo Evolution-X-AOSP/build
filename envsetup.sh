d933f7640835cc77741ee686bd46c1e8991b9db0
xplodwild <xplod@ouverta.fr>
2013-08-24 17:40:37 +0100

    Add mka alias
    Change-Id: I4c057d3f59978dd5c20452b334d94c98ebc22659
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- envsetup.sh
+++ envsetup.sh
@@ -1618,6 +1618,18 @@ function _complete_android_module_names() {
     COMPREPLY=( $(QUIET_VERIFYMODINFO=true allmod | grep -E "^$word") )
 }
 
+# Make using all available CPUs
+function mka() {
+    case `uname -s` in
+        Darwin)
+            make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
+            ;;
+        *)
+            schedtool -B -n 1 -e ionice -n 1 make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
+            ;;
+    esac
+}
+
 # Print colored exit condition
 function pez {
     "$@"
