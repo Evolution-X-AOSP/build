f018760414ae1088d948ea0d2c1f81e40f5147ab
Koushik Dutta <koushd@gmail.com>
2017-08-23 16:13:18 +0200

    Add roomservice
    Partial squash of:
    
      Author: Luca Stefani <luca.stefani.ge1@gmail.com>
      Date: Sun, 1 Jan 2017 21:45:07 +0100
    
        lunch: Use cd - instead of popd
    
        * ZSH breaks a lot of things.
    
        Change-Id: Idee8485bee673bebf6fe5857253306b20aa92eb5
    
      Author: Luca Stefani <luca.stefani.ge1@gmail.com>
      Date: Thu, 5 Jan 2017 16:30:15 +0100
    
        build: Replace pushd with cd
    
        * For consistency
    
        Change-Id: I96711e3be7b73ebadd589f460e5c5e90d000a4f7
    
      Author: Koushik Dutta <koushd@gmail.com>
      Date: Sat, 26 Nov 2011 18:51:42 -0800
    
        roomservice delivers you lunch combos from the CyanogenMod github.
    
        fix roomservice formatting
        support product names with _ in them
        fix roomservice to handle pagination
    
        Change-Id: I4923c2f768094dbad8e06a72d9f27d46414030ab
    
      Author: LuK1337 <priv.luk@gmail.com>
      Date:   Tue Apr 13 19:57:24 2021 +0200
    
        envsetup: Fix the way we check check_product's return value
    
        Change-Id: I5a2a4b7be5dfee198094902aaa2c9a8c27b97704
    
    Change-Id: I5bb07944f558e3e5551ab65c97ecfa303609be57
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- envsetup.sh
+++ envsetup.sh
@@ -767,7 +767,20 @@ function lunch()
         return 1
     fi
 
-    check_product $product
+    if ! check_product $product
+    then
+        # if we can't find a product, try to grab it off the Evolution X GitHub
+        T=$(gettop)
+        cd $T > /dev/null
+        vendor/evolution/build/tools/roomservice.py $product
+        cd - > /dev/null
+        check_product $product
+    else
+        T=$(gettop)
+        cd $T > /dev/null
+        vendor/evolution/build/tools/roomservice.py $product true
+        cd - > /dev/null
+    fi
 
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
