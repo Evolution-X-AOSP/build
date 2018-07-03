d3ceefcbdceb9c4c3597a512fea0d85620982b1c
ganeshi4u <ganeshvarmai4u@gmail.com>
2018-07-03 08:47:11 +0200

    env: add repopick trigger
    Change-Id: Ib49d9450b935deb72bca602a3f33ee43742f1b46
    (cherry picked from commit fbe7618184a6ff6ecbc49eee976b7b43055f6c4f)

--- envsetup.sh
+++ envsetup.sh
@@ -1846,3 +1846,10 @@ function showcommands() {
 validate_current_shell
 source_vendorsetup
 addcompletions
+
+export ANDROID_BUILD_TOP=$(gettop)
+
+function repopick() {
+    T=$(gettop)
+    $T/vendor/evolution/build/tools/repopick.py $@
+}
