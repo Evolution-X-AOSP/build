7d9de8057c8f9f92895b452d92b560c111e8df43
LuK1337 <priv.luk@gmail.com>
2024-03-11 11:28:46 +0100

    envsetup: Pass valid TARGET_RELEASE to check_product
    Change-Id: I709248c0d362cffdac57d9893b7dfb4d2329204c

--- envsetup.sh
+++ envsetup.sh
@@ -222,7 +222,7 @@ function check_product()
     export EVOLUTION_BUILD
 
         TARGET_PRODUCT=$1 \
-        TARGET_RELEASE= \
+        TARGET_RELEASE=$2 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
         TARGET_BUILD_APPS= \
@@ -833,14 +833,14 @@ function lunch()
         return 1
     fi
 
-    if ! check_product $product
+    if ! check_product $product $release
     then
         # if we can't find a product, try to grab it off the Evolution X GitHub
         T=$(gettop)
         cd $T > /dev/null
         vendor/evolution/build/tools/roomservice.py $product
         cd - > /dev/null
-        check_product $product
+        check_product $product $release
     else
         T=$(gettop)
         cd $T > /dev/null
